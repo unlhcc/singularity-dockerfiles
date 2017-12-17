dbaed51a69425d689625f6deb7103e443589ded0
Natasha Pavlovikj <npavlovikj2@unl.edu>
2017-12-17 08:08:44 +0000

    Change the test command

--- dparsf/test.sh
+++ dparsf/test.sh
@@ -1,1 +1,1 @@
-imageCommand="test -e /opt/DPARSFA_run_StandAlone_Linux/DPARSFA_run_mcr/home/yanlab/Soft/DPABI/DPABIV2.3StandAlone/DPARSFA_run_SeparateDir/DPARSFA_run.m"
+imageCommand="test -d /opt/DPARSFA_run_StandAlone_Linux"
