1d412bfb8c6f992c12a594bcdfaf63eb3223bd56
Adam Caprez <acaprez2@unl.edu>
2018-02-14 18:28:42 +0000

    Remove Singularity Docker cache between tests.
    [skip ci]
    Temporary workaround to avoid running the test Docker container
    out of space.

--- testall.sh
+++ testall.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 imageList=`ls -1d */ | cut -f1 -d '/'`
 for image in $imageList
 do
+    rm -rf ~/.singularity/docker/*
     source ${image}/test.sh
     testCommand="singularity exec docker://$REGISTRY_HOSTNAME:$REGISTRY_PORT/$HCC_REPO_NAMESPACE/${image} ${imageCommand}"
     echo "Running test command '$testCommand'"
